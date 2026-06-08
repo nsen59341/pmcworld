f369dd1898f70e59a30749dac81a45ecc6e33359
Natashasen <nsen59341@gmail.com>
2026-06-08 23:19:35 +0530

    Fixed Youtube link

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -65,7 +65,7 @@ export function Navigation() {
 
               {/* YouTube */}
               <a
-                href="https://www.youtube.com/@pmcworld"
+                href="https://www.youtube.com/@pmcworldd"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="text-gray-400 hover:text-[#FF0000] transition-colors"
